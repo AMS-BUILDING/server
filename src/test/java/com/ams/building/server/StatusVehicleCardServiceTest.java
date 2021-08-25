46b0db1bd0b5a924a144f6dc7de360c6aa747693
Nguyễn Minh Tuấn <65123657+loidv01071999@users.noreply.github.com>
2021-08-25 21:16:38 +0700

    update UT

---
+++ src/test/java/com/ams/building/server/StatusVehicleCardServiceTest.java
@@ -0,0 +1,49 @@
+package com.ams.building.server;
+
+import com.ams.building.server.bean.StatusVehicleCard;
+import com.ams.building.server.dao.StatusVehicleCardDAO;
+import com.ams.building.server.response.StatusVehicleCardResponse;
+import com.ams.building.server.service.impl.StatusVehicleCardServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(PowerMockRunner.class)
+public class StatusVehicleCardServiceTest {
+
+
+    @Mock
+    private StatusVehicleCardDAO statusVehicleCardDAO;
+
+    @InjectMocks
+    StatusVehicleCardServiceImpl statusVehicleCardServiceImpl;
+
+    StatusVehicleCard statusVehicleCard = new StatusVehicleCard(1L ,"name");
+
+    List<StatusVehicleCard> statusVehicleCards = Arrays.asList(statusVehicleCard);
+
+    @Test
+    public void listStatus() {
+
+        Mockito.when(statusVehicleCardDAO.findAll()).thenReturn(statusVehicleCards);
+        List<StatusVehicleCardResponse> responses = new ArrayList<>();
+        statusVehicleCards.forEach(s -> responses.add(convertToStatusResponse(s)));
+        statusVehicleCardServiceImpl.listStatus();
+    }
+
+
+    private StatusVehicleCardResponse convertToStatusResponse(StatusVehicleCard card) {
+        StatusVehicleCardResponse response = StatusVehicleCardResponse.builder().build();
+        response.setId(card.getId());
+        response.setStatusName(card.getStatusName());
+        return response;
+    }
+}
