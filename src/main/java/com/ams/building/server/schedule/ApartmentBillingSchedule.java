0c467b5d199b0886534eccd9fccf42e9bccacf0d
HangNT164 <hangnt16499@gmail.com>
2021-08-30 00:48:57 +0700

    Fix bug

--- src/main/java/com/ams/building/server/schedule/ApartmentBillingSchedule.java
+++ src/main/java/com/ams/building/server/schedule/ApartmentBillingSchedule.java
@@ -16,8 +16,12 @@ public class ApartmentBillingSchedule {
     @Autowired
     private ApartmentBillingService service;
 
-    //    @Scheduled(cron = "1 * * * * ?")
-//    @Scheduled(cron = "0 0 1 * * ?")
+    /**
+     * Check And Auto Insert Billing In Month.
+     * In the 1rd when 00:00 AM will auto insert to calculate billing for resident
+     */
+    //@Scheduled(cron = "1 * * * * ?")
+    @Scheduled(cron = "0 0 1 * * ?")
     public void checkAndInsertBillingInMonth() {
         try {
             service.checkAndInsertBillingInMonth();
@@ -45,7 +49,7 @@ public class ApartmentBillingSchedule {
      * Mail will send 5rd in month.
      */
     @Scheduled(cron = "0 9 5 * * ?")
-//    @Scheduled(cron = "2 * * * * ?")
+//    @Scheduled(cron = "1 * * * * ?")
     public void sendMailRemindForResident() {
         try {
             service.sendMailRemindForResident();
